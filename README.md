8303f3258a5ff620af00411b9919021b9365e108
Rob Blackwell <rob.blackwell@aws.net>
2011-08-31 09:14:30 +0100

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 node-azure
 ----------
 
-A proof of concept Azure Storage library for Node.js
+A Node.js library for Accessing Windows Azure Storage.
 
+Embryonic and incomplete, but proves the concepts and looks like a good
+basis for further work.
 
+Any feedback / patches gratefully received
+
+Depends on xml2js which can be installed via NPM
+  
+Rob Blackwell
+August 2011
+
+TODO
+
+Implement the wider APIs
+Make it NPM installable
+Implement a parser for Azure connection strings
+Implement a queue reader that looks like a callback
+Implement other Azure APIs? AppFabric? Service Management?
--- test/azure-test.js
+++ test/azure-test.js
@@ -1,8 +1,11 @@
 var assert = require('assert');
 var azure = require('../lib/azure.js');
 
-function test_hmac_string() 
-{
+// Use this account for testing ..
+var test_account = azure.devstore_account;
+
+function test_hmac_string () {
+  
 	var key = 'GuGbCQ41a9G1vtS1/dairlSMbXhHVzoA8+VPrbWxtj94o0aoAQdsgaaoYQASWqG9mj8xDvP1hSkvSVcLC34CfA==';
 
 	var msg = "Hello World";
@@ -12,11 +15,10 @@ function test_hmac_string()
 	assert.equal(actual, expected, 'hmac_string test failed');
 }
 
-
 test_hmac_string() ;
 
-function test_canonicalized_resource_format_1 ()
-{
+function test_canonicalized_resource_format_1 () {
+  
 	var account = {
 		name: 'myaccount',
 		key : 'x'
@@ -35,18 +37,58 @@ function test_canonicalized_resource_format_1 ()
 
 test_canonicalized_resource_format_1();
 
-function test_canonicalized_headers()
-{
+function test_canonicalized_headers () {
+  
 	var req = {
-		url : "http://robblackwell.blob.core.windows.net/?comp=list&restype=container",
+		url : "http://myaccount.blob.core.windows.net/?comp=list&restype=container",
 		headers: {'x-ms-version' : '2009-09-19','x-ms-date' : 'Sun, 12 Jun 2011 10:00:45 GMT'}
 	}
 	var actual = azure.canonicalized_headers(req);
-	console.log(":::" + actual);
 	var expected =  "x-ms-date:Sun, 12 Jun 2011 10:00:45 GMT\nx-ms-version:2009-09-19\n";
 	assert.equal(actual,expected, 'canonicalized_headers failed');
 }
 
-
 test_canonicalized_headers();
+
+function test_container () {
+  
+	azure.create_container(test_account, "foobar3", 
+			function(x) 
+			{ 
+				azure.delete_container(test_account, "foobar3", 
+					function(x) { assert.ok(azure.accepted(x),"test_container failed")});
+			});
+}
+
+test_container();
+
+function test_list_containers () {
+	azure.list_containers(test_account,
+                              function(x) { assert.ok(azure.ok(x),"test_list_containers failed")});
+}
+                              
+test_list_containers();
+
+function test_list_tables () {
+	azure.list_tables(test_account,
+                              function(x) { assert.ok(azure.ok(x),"test_list_tables failed")});
+}
+                              
+test_list_tables();
+
+function test_list_queues () {
+	azure.list_queues(test_account,
+                              function(x) { assert.ok(azure.ok(x),"test_list_queues failed")});
+}
+                              
+test_list_queues();
+
+//azure.get_container_properties(test_account, "packages", azure.show_response);
+//azure.get_container_metadata(test_account, "packages", azure.show_response);
+//azure.list_blobs(test_account, 'packages');
+//azure.get_blob(test_account, 'packages', 'ed-isla.JPG', azure.show_response);
+//azure.download_blob(test_account, 'packages', 'ed-isla.JPG', "d:\\junk\\foo.jpg");
+
+
+
 
